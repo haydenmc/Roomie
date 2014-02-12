7bf9b3147c6a2b0dda3919e2d97753f083e2197a
Hayden McAfee <hayden@outlook.com>
2014-02-12 10:38:32 -0500

    Changed bundle paths to (hopefully) fix path conflicts on Azure deploy.

--- RoomieWeb/App_Start/BundleConfig.cs
+++ RoomieWeb/App_Start/BundleConfig.cs
@@ -10,12 +10,12 @@ namespace RoomieWeb
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/Scripts/ts").Include(
+			bundles.Add(new ScriptBundle("~/bundle/Scripts/ts").Include(
 				"~/Scripts/ts/application.js",
 				"~/Scripts/ts/page.js",
 				"~/Scripts/ts/pages/login.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
+			bundles.Add(new StyleBundle("~/bundle/Content/css").Include(
 				 "~/Content/css/app.css",
 				 "~/Content/css/animation.css"));
 		}
