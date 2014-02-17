c6c53a880ea037fa44bb739afc7a09dc48fdf0a5
Hayden McAfee <hayden@outlook.com>
2014-02-17 17:01:45 -0500

    Registration page has an actual form now. Needs tweaks.

--- RoomieWeb/Scripts/ts/pages/register.ts
+++ RoomieWeb/Scripts/ts/pages/register.ts
@@ -1,5 +1,13 @@
 class Register extends Page {
 	constructor() {
 		super("Register");
+
+		// Generate Registration form
+		var form = document.createElement("div");
+		form.id = "Register";
+		form.innerHTML = '<form class="register"><input type="email" name="email" placeholder="e-mail address" /><br /><input type="text" name="displayname" placeholder="display name" /><br /><input type="password" name="password" placeholder="password" /><br /><input type="password" name="confirmpassword" placeholder="confirm password" /><input type="submit" value="register" /></form>';
+
+		// Insert into page.
+		this.page_element.insertBefore(form, null);
 	}
 } 
