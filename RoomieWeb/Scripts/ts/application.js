07df5b00f6dc4a90ca6922d46e31cba1adf1a88a
Lars <larssorenson@outlook.com>
2014-03-14 14:09:01 -0400

    New hash function also I'm a girl

--- RoomieWeb/Scripts/ts/application.js
+++ RoomieWeb/Scripts/ts/application.js
@@ -169,8 +169,8 @@ String.prototype.hashCode = function () {
         return hash;
     for (var i = 0; i < this.length; i++) {
         var char = this.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32bit integer
+        hash += char;
+        hash %= 360;
     }
     return hash;
 };
--- RoomieWeb/Scripts/ts/application.ts
+++ RoomieWeb/Scripts/ts/application.ts
@@ -179,8 +179,8 @@ function htmlEscape(str) {
 	if (this.length == 0) return hash;
 	for (var i = 0; i < this.length; i++) {
 		var char = this.charCodeAt(i);
-		hash = ((hash << 5) - hash) + char;
-		hash = hash & hash; // Convert to 32bit integer
+		hash += char;
+        hash %= 360;
 	}
 	return hash;
 }
